f594e91f5746235029f82f47b560006785a8e110
tejalpagar948 <tejalpagar948@gmail.com>
2026-04-13 07:46:49 +0530

    api issue fix on production

--- src/api/client.js
+++ src/api/client.js
@@ -1,15 +1,11 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log('ENV BASE URL:', BASE_URL);
+
 export async function apiClient(endpoint) {
   const url = `${BASE_URL}${endpoint}`;
-
-  console.log('API CALL:', url);
+  console.log('FINAL URL:', url);
 
   const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error(`API Error: ${response.status}`);
-  }
-
   return await response.json();
 }
